8a92fb2ce549716251bc70d42c6324f2e98b2e55
fanuelal <fanuelalmawstop@gmail.com>
2024-03-12 13:46:40 +0300

    elevated button widget

--- lib/screens/auth_type.dart
+++ lib/screens/auth_type.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../config/theme.dart';
-import 'package:google_fonts/google_fonts.dart';
+import '../widget/Button.dart';
+
 
 class AuthType extends StatefulWidget {
   const AuthType({super.key});
@@ -12,26 +12,12 @@ class AuthType extends StatefulWidget {
 class _AuthTypeState extends State<AuthType> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     return Column(
           children: [
-    AspectRatio(
-      aspectRatio: 9/16,
-      child: Image.asset('assets/logo_2.png')),
-    ElevatedButton(
-        style: ButtonStyle(
-            maximumSize: MaterialStateProperty.all<Size>(
-                Size(size.width, size.height)),
-            textStyle: MaterialStateProperty.all<TextStyle>(GoogleFonts.inter(fontSize: 16)),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32))),
-            foregroundColor:
-                MaterialStateProperty.all<Color>(ColorTheme.white),
-            backgroundColor:
-                MaterialStateProperty.all<Color>(ColorTheme.primaryColor)),
-        onPressed: () {},
-        child: const Text('hi I am here'))
+    Image.asset('assets/logo.png'),
+    // Button(height: 0.1, width: 0.5, onTap: () {}, isElevated: false),
+    Button(height: 0.1, width: 0.5, onTap: () {},)
           ],
         );
   }
